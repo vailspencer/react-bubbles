5f168631409174f2d8de8f4a71c2eff00fa77923
Spencer_Vail <vail.spencer@gmail.com>
2020-05-22 09:14:39 -0700

    privateroute and login completed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
 import React from "react";
+import { useForm } from 'react-hook-form';
+import axiosWithAuth from '../utils/AxiosWithAuth.js';
+import { useHistory } from 'react-router-dom';
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+const { register, handleSubmit, reset} = useForm();
+const history = useHistory();
+
+
+const onSubmit = (data) => {
+  setTimeout(() => {
+    axiosWithAuth()
+    .post('/login', data)
+    .then((res) => {
+      localStorage.setItem('token', res.data.payload);
+      history.push('/bubbles');
+    })
+    .cath((err) => console.error(err));
+    reset();
+  }, 1000);
+};
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit = {handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor = 'username'> Username: </label>
+          <input
+            type = 'text'
+            ref ={register}
+            name = 'username'
+            placeholder = 'enter username'          
+          />          
+        </div>
+        <div>
+          <label htmlFor='password'>Username:</label>
+          <input 
+            type='password'
+            ref = {register}
+            name ='password'
+            placeholder = 'Please enter your password'
+          />
+        </div>
+        <button type = 'submit'>Login</button>
+      </form>
     </>
   );
 };
--- client/src/utils/AxiosWithAuth.js
+++ client/src/utils/AxiosWithAuth.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosWithAuth = () => {
-    const token = lockalStorage.getItem('token')
+    const token = localStorage.getItem('token')
 
     return axios.create({
         baseURL: 'http://localhost:5000/api',
